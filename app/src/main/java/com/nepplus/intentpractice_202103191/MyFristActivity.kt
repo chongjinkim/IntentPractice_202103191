205f47b823f989904598c5e1937b921d05ce29d5
김종진 <martia0118@gmail.com>
2021-03-19 01:06:19 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/intentpractice_202103191/MyFristActivity.kt
@@ -0,0 +1,11 @@
+package com.nepplus.intentpractice_202103191
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyFristActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_frist)
+    }
+}
